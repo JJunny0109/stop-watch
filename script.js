be7175abd543a8801009fa072ca95acdbc74f751
fakeArmy <jjunny0109@gmail.com>
2023-08-13 23:56:40 +0900

    feat : Make startStopwatch function
    - just make start button feature
    - cannot stop or reset the stopwatch
    - have to make pause and reset functions

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let startTime = 0; // 시간 값, 초기값은 0
 let intervalId; // 타이머 간격을 저장할 변수
 let isRunning = false; //스톱워치가 시작되어있는지를 확인하는 변수
 
+
 // 이벤트 핸들러 추가
 // startPause 버튼 클릭 시
 startPauseBtn.addEventListener("click", () => {
@@ -30,13 +31,40 @@ resetBtn.addEventListener("click", () => {
 // 스톱워치 함수 구현
 function startStopwatch() {
   startTime = Date.now(); // 버튼 클릭 시점의 시간으로 설정
-  intervalId = setInterval(updateTime, 1000); // 1초마다 updateTime 함수 호출
+  intervalId = setInterval(updateTime, 10); // 1초마다 updateTime 함수 호출
 }
 
 // 시간 업데이트 함수
 function updateTime() {
-  const currentTime = Date.now(); // 현재 시간을 가져옴
+  const currentTime = Date.now(); // 현재 시각을 가져옴
   const elapsedTime = currentTime - startTime; // 경과된 시간 계산
-  console.log(elapsedTime);
+  // console.log(elapsedTime);
+
   // elapsedTime를 화면에 표시하기 위해 분:초 형식으로 변환 후 화면 업데이트
+  formatElapsedTime(elapsedTime);
+}
+
+// 밀리초를 시, 분, 초, 밀리초로 변환하는 함수
+function formatElapsedTime(Elapsed_Time) {
+  const hours = parseInt(Elapsed_Time / 3600000);
+  const minutes = parseInt((Elapsed_Time % 3600000) / 60000);
+  const seconds = parseInt((Elapsed_Time % 60000) / 1000);
+  const milliseconds = Elapsed_Time % 1000;
+  // console.log(formatTime(hours, minutes, seconds, milliseconds));
+
+  // 시간 형식 표시
+  stopwatchDisplay.innerHTML = formatTime(hours, minutes, seconds, milliseconds);
+}
+
+// 시, 분, 초, 밀리초를 입력 받아 -> 00:00:00.00 형식으로 출력하는 함수
+function formatTime(Hours, Minutes, Seconds, Milliseconds) {
+  const formattedHours = Hours.toString().padStart(2, '0');
+  const formattedMinutes = Minutes.toString().padStart(2, '0');
+  const formattedSeconds = Seconds.toString().padStart(2, '0');
+  
+  // 밀리초가 3자리 수 이므로 이를 다시 2자리 수로 변환
+  const formattedMilliseconds0 = Milliseconds.toString().padStart(2, '0');
+  const formattedMilliseconds = formattedMilliseconds0.substr(0, 2);
+
+  return `${formattedHours}:${formattedMinutes}:${formattedSeconds}.${formattedMilliseconds}`;
 }
